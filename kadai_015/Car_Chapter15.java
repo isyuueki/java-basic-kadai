247a4fdbd463dce98191b0c573766d3f8bb1c243
isyuueki <isyuueki@gmail.com>
2025-03-06 21:20:43 +0800

    kadai_015完成

---
+++ kadai_015/Car_Chapter15.java
@@ -0,0 +1,24 @@
+package kadai_015;
+
+// 商品クラス
+public class Car_Chapter15 {
+    private int gear = 1;
+    private int speed = 0;
+
+    public void changeGear(final int afterGear) {
+        System.out.println("ギア" + this.gear + "から" + afterGear + "に切り替えました");
+        this.gear = afterGear;
+        this.speed = switch (afterGear) {
+            case 1 -> 10;
+            case 2 -> 20;
+            case 3 -> 30;
+            case 4 -> 40;
+            case 5 -> 50;
+            default -> 10;
+        };
+    }
+
+    public void run() {
+        System.out.println("速度は時速" + this.speed + "kmです");
+    }
+}
